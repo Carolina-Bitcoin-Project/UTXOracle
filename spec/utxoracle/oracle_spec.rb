c9ee2acf368772336519468141cbce67615fd2b6
kg <keithgardner@icloud.com>
2023-10-31 21:01:25 -0400

    rubocop

--- spec/utxoracle/oracle_spec.rb
+++ spec/utxoracle/oracle_spec.rb
@@ -2,22 +2,20 @@
 
 require 'spec_helper'
 
-=begin
-RSpec.describe Utxoracle::Oracle do
-  let(:oracle) do
-    described_class.new('aUser', 'aPassword', '127.0.0.1', '8332')
-  end
-
-  describe '.new' do
-    it 'initialized an Rpc instance' do
-      expect(Utxoracle::Rpc).to receive(:new)
-      described_class.new('aUser', 'aPassword', '127.0.0.1', '8332')
-      expect(oracle.class).to eq Utxoracle::Oracle
-    end
-  end
-
-  # TODO: - test cache
-  # TODO - test provider interface
-  # TODO - test `price` return type
-end
-=end
+# RSpec.describe Utxoracle::Oracle do
+#   let(:oracle) do
+#     described_class.new('aUser', 'aPassword', '127.0.0.1', '8332')
+#   end
+#
+#   describe '.new' do
+#     it 'initialized an Rpc instance' do
+#       expect(Utxoracle::Rpc).to receive(:new)
+#       described_class.new('aUser', 'aPassword', '127.0.0.1', '8332')
+#       expect(oracle.class).to eq Utxoracle::Oracle
+#     end
+#   end
+#
+#   # TODO: - test cache
+#   # TODO - test provider interface
+#   # TODO - test `price` return type
+# end
